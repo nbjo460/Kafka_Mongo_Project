f1ac08cf965c83aad0889143c46c87c76340d2b7
menachem yarhi <nbjo460@gmail.com>
2025-08-25 17:47:25 +0300

    route finish

--- Subscribers/routes.py
+++ Subscribers/routes.py
@@ -1,18 +1,28 @@
 import fastapi
+from dal import Dal
 
 app = fastapi.FastAPI()
 
 
 class Routes:
     def __init__(self):
-        pass
+        self.dal = Dal()
+
     @app.get("/")
     def root(self):
-        return {}
-    @app.get("/load")
-    def load_db(self):
+        return {"status" : "HELLO"}
+    @app.get("/get-interesting")
+    def load_interesting(self):
         """
-        For load the data.
-        :return:
+        In route: "/get-interesting", load the interesting messages.
+        :return: json
         """
-        return Manager().get_data()
+        return self.dal.get_all_messages_interesting()
+
+    @app.get("/get-not-interesting")
+    def load_not_interesting(self):
+        """
+        In route: "/get-not-interesting", load the not interesting messages.
+        :return: json
+        """
+        return self.dal.get_all_messages_not_interesting()
